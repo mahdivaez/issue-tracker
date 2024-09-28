580ad2a0cf9e88a258cba16abd0318cb2cd5f2f9
Mehdi Vaez <mahdivaeztehrani81@gmail.com>
2024-09-28 09:03:42 -0700

    buils api for creating

---
+++ app/api/issues/route.ts
@@ -0,0 +1,25 @@
+import { json } from 'stream/consumers'
+import { z } from 'zod'
+import prisma from '@/prisma/client'
+const createIssueSchema = z.object({
+
+    title: z.string().min(1).max(255),
+    description: z.string().min(1)
+})
+
+export async function POST(request: Request) {
+
+    const body = await request.json()
+    const validation = createIssueSchema.safeParse(body)
+
+    if (!validation.success) return new Response(JSON.stringify(validation.error.errors), { status: 400 })
+
+    const newIssue = await prisma.issue.create({
+        data : {
+            title : body.title , description : body.description 
+        }
+    })
+
+    return new Response(JSON.stringify(newIssue) , {status :  201})
+
+}
--- app/issues/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Issuepage = () => {
-  return (
-    <div>Issuepage</div>
-  )
-}
-
-export default Issuepage

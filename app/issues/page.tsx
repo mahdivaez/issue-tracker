320c52dd35243bc0010dbcdabf567e83aad340b9
Mehdi Vaez <mahdivaeztehrani81@gmail.com>
2024-09-28 09:20:39 -0700

    set up radix-UI

---
+++ app/issues/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import  {Button} from "@radix-ui/themes"
+const Issuepage = () => {
+  return (
+
+        <div className='text-black'><Button>New Issue</Button></div>
+    )
+}
+
+export default Issuepage
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './NavBar'
+import { Theme } from '@radix-ui/themes';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,12 +20,16 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {/* Navbar should be outside of the main content */}
+      <Theme>
+
         <Navbar/>
         <main>
 
           {children}
 
         </main>
+      </Theme>
+
       </body>
     </html>
   )

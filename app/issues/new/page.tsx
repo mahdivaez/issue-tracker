97e50fa94c80445a89e5a325e4e2fe8c9c168665
Mehdi Vaez <mahdivaeztehrani81@gmail.com>
2024-09-29 07:29:29 -0700

    Refactor : Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -8,12 +8,12 @@ import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchema';
-import {TypeOf, z} from "zod"
+import { TypeOf, z } from "zod"
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false });
 
-type IssueForm = z.infer<typeof createIssueSchema> 
+type IssueForm = z.infer<typeof createIssueSchema>
 // interface IssueForm {
 //     title: string;
 //     description: string;
@@ -21,45 +21,46 @@ type IssueForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
     const router = useRouter();
-    const { register, control, handleSubmit ,formState :{errors} } = useForm<IssueForm>({
-        resolver : zodResolver(createIssueSchema)
+    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
     });
     const [error, setError] = useState("");
-const [isSubmitting , setSubmitting] = useState(false)
+    const [isSubmitting, setSubmitting] = useState(false)
+    const onSubmit = handleSubmit(async (data) => {
+        try {
+            setSubmitting(true)
+            const response = await axios.post("/api/issues", data);
+
+            if (response.status === 400) {
+                // If there's a validation error, set the error message
+                const errors = response.data.errors.map((err: any) => err.message).join(", ");
+                setError(errors);
+            } else {
+                router.push("/issues");
+            }
+        } catch (error) {
+            setSubmitting(false)
+            setError("Something went wrong");
+        }
+    })
 
 
     return (
         <div>
             {error && (
-    <Callout.Root color='red' className='mb-4 p-5 bg-red-50 rounded-xl max-w-xl'>
-        <Callout.Icon>
-            {/* You can place an icon here, for example, an error icon */}
-        </Callout.Icon>
-        <Callout.Text className="text-red-500"> {/* Apply the color class here */}
-            {error}
-        </Callout.Text>
-    </Callout.Root>
-)}
-        
+                <Callout.Root color='red' className='mb-4 p-5 bg-red-50 rounded-xl max-w-xl'>
+                    <Callout.Icon>
+                        {/* You can place an icon here, for example, an error icon */}
+                    </Callout.Icon>
+                    <Callout.Text className="text-red-500"> {/* Apply the color class here */}
+                        {error}
+                    </Callout.Text>
+                </Callout.Root>
+            )}
+
             <form
                 className='max-w-xl space-y-10'
-                onSubmit={handleSubmit(async (data) => {
-                    try {
-                        setSubmitting(true)
-                        const response = await axios.post("/api/issues", data);
-
-                        if (response.status === 400) {
-                            // If there's a validation error, set the error message
-                            const errors = response.data.errors.map((err: any) => err.message).join(", ");
-                            setError(errors);
-                        } else {
-                            router.push("/issues");
-                        }
-                    } catch (error) {
-                        setSubmitting(false)
-                        setError("Something went wrong");
-                    }
-                })}
+                onSubmit={onSubmit}
             >
                 <TextField.Root className='border p-2 rounded-xl text-black' placeholder="Title" {...register("title")}>
                 </TextField.Root>
@@ -74,7 +75,7 @@ const [isSubmitting , setSubmitting] = useState(false)
                             className='border rounded-xl p-3 w-full text-gray-700 placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:shadow-sm'
                             placeholder='Enter Description'
                         />
-                        
+
                     )}
                 />
                 {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
@@ -82,7 +83,7 @@ const [isSubmitting , setSubmitting] = useState(false)
 
                 <Button disabled={isSubmitting} className='w-full bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300 transition'>
                     Submit New Issue
-                    {isSubmitting && <Spinner/>}
+                    {isSubmitting && <Spinner />}
                 </Button>
             </form>
         </div>

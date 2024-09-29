8866889227cfbdae4619832e66bfdb61b4d06f8a
Mehdi Vaez <mahdivaeztehrani81@gmail.com>
2024-09-29 02:51:35 -0700

    costomize radix ui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './NavBar'
-import { Theme } from '@radix-ui/themes';
+import { Theme, ThemeContext, ThemePanel } from '@radix-ui/themes';
 
 const inter = Inter({ subsets: ['latin'] })
 
